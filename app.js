6486c3eef369a83d3001b77654d4f51cead601b4
Jessica_Sugianto <jezicalove12@gmail.com>
2020-03-27 01:57:21 +0700

    memperbaiki bentrok

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const router = require('./routes/route');
-<<<<<<< HEAD
 const multer = require('multer');
 const path = require('path')
-=======
 const session = require('express-session')
->>>>>>> jessica-sugianto
 
 const storage = multer.diskStorage({
     destination: './public/uploads/',
-    filename: function (req, file, cb) {
+    filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
 })
@@ -23,16 +20,13 @@ const upload = multer({
 app.locals.helperPrice = require('./helpers/helperPrice');
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-<<<<<<< HEAD
 app.use(express.static('./public'))
-=======
 app.use(session({
     secret: 'TB Sencho',
     resave: true,
     saveUninitialized: true
 }))
 
->>>>>>> jessica-sugianto
 app.use('/', router);
 
 
--- routes/book.js
+++ routes/book.js
@@ -4,6 +4,8 @@ const bookController = require('../controllers/book')
 
 router.get('/', bookController.list);
 router.get('/search', bookController.search);
+
+router.get('/view/:id', bookController.view);
 router.use((req, res, next) => {
     if (req.session.role === 'Admin') {
         next()
@@ -15,15 +17,10 @@ router.use((req, res, next) => {
 })
 router.get('/add', bookController.create);
 router.post('/addDataPostgree', bookController.add);
-router.get('/view/:id', bookController.view);
 router.get('/edit/:id', bookController.edit);
 router.post('/edit/:id', bookController.editData);
-<<<<<<< HEAD
-router.get('/search', bookController.search);
 router.get('/seeCustomer/:id', bookController.seeCustomer);
 router.get('/upload/:id', bookController.uploadForm);
 router.post('/upload/:id', bookController.uploadData)
-=======
->>>>>>> jessica-sugianto
 
 module.exports = router;
